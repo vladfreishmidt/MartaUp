56d9ffc8fa435f138d121fe42f1bcd438a5089d6
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-01-10 21:39:53 +0200

    Header slider and Services section completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(function () {
+    $('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header-dots',
+        autoplay: 2000
+    });
 });
